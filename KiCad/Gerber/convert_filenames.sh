6e52598f1776128e6214d6ab4e16053fba2be78f
Kelly McDonald <kelly@fammcdonald.net>
2019-07-08 11:12:40 -0400

    Add Last send of Circuit board

--- KiCad/Gerber/convert_filenames.sh
+++ KiCad/Gerber/convert_filenames.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-mv Helios-B.Cu.gbr Helios.GBL
-mv Helios-F.Cu.gbr Helios.GTL
-mv Helios-F.Mask.gbr Helios.GTS
-mv Helios-B.Mask.gbr Helios.GBS
-mv Helios-F.SilkS.gbr Helios.GTO
-mv Helios-B.SilkS.gbr Helios.GBO
-mv Helios-Edge.Cuts.gbr Helios.GML
-mv Helios.drl Helios.TXT
-zip HeliosCompiled.zip Helios.*
+mv Bonaire-B.Cu.gbr Bonaire.GBL
+mv Bonaire-F.Cu.gbr Bonaire.GTL
+mv Bonaire-F.Mask.gbr Bonaire.GTS
+mv Bonaire-B.Mask.gbr Bonaire.GBS
+mv Bonaire-F.SilkS.gbr Bonaire.GTO
+mv Bonaire-B.SilkS.gbr Bonaire.GBO
+mv Bonaire-Edge.Cuts.gbr Bonaire.GML
+mv Bonaire.drl Bonaire.TXT
+zip BonaireCompiled.zip Bonaire.*
